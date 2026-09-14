0e216f47505e96c104bebbc36c5aa830f11c9003
mayankks01 <159142818+mayankks01@users.noreply.github.com>
2026-09-14 15:55:50 +0530

    Add project files.

---
+++ Controllers/RandomController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RandomController : ControllerBase
+    {
+        [HttpGet]
+        public string Random()
+        {
+            return "Random";
+        } 
+    }
+}
